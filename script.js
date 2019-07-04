660f7e5bc90e5eaa8a1bf694ce6e57699b19d50d
giger2000 <alberto.anuncio@gmail.com>
2019-07-04 10:01:51 +0200

    Script per multiplicar 2 números
    funció JS que rep 2 paràmetres i els multiplica. Retorn valor

---
+++ script.js
@@ -0,0 +1,13 @@
+function producte(param1,param2){
+   return param1*param2;
+}
+
+
+
+var numUsuari1=prompt("Dame el primer número");
+var numUsuari2=prompt("Dame el primer número");
+var resultatMultiplicacio=producte(numUsuari1,numUsuari2);
+
+
+alert("el valor de la primera multiplicació és: "+resultatMultiplicacio);
+
